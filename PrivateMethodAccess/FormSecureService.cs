f0200b6db07211ec4eca9aa735e3e046cd269a9c
Leandro M Machado <leandro-m-m@hotmail.com>
2026-04-02 16:40:24 -0300

    modifying IsAuthorized method

--- PrivateMethodAccess/FormSecureService.cs
+++ PrivateMethodAccess/FormSecureService.cs
@@ -1,16 +1,17 @@
+using System.Security.Claims;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PrivateMethodAccess;
 
 public partial class FormSecureService: Form
 {
-    public delegate string AuthorizedAction();
+    public delegate string AuthorizedAction();   
 
     public FormSecureService()
-    {
+    {       
         InitializeComponent();
     }
-
+   
     // Private method we want to protect
     private string PerformSecureAction()
     {
@@ -29,11 +30,29 @@ public partial class FormSecureService: Form
         }
     }
 
-    private bool IsAuthorized(string callerId)
+    private bool IsAuthorized(string role)
     {
         // Simulated authorization logic
-        var authorizedCallers = new HashSet<string> { "Admin", "TrustedService" };
-        return authorizedCallers.Contains(callerId);
+        //var authorizedCallers = new HashSet<string> { "Admin", "TrustedService" };
+        //return authorizedCallers.Contains(callerId);
+
+        string username = "AdminUser";
+        var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Email, $"{username}@example.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+        ClaimsIdentity identity = new ClaimsIdentity(claims, "TestAuth");
+        ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+        // Get the role claim and compare with the role parameter
+        var roleClaim = claimsPrincipal.Claims
+            .FirstOrDefault(c => c.Type == ClaimTypes.Role);
+
+        return roleClaim != null && roleClaim.Value == role;
     }
 
     private void button1_Click(object sender, EventArgs e)
--- PrivateMethodAccess/Program.cs
+++ PrivateMethodAccess/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+
 namespace PrivateMethodAccess
 {
     internal static class Program
@@ -7,9 +9,7 @@ namespace PrivateMethodAccess
         /// </summary>
         [STAThread]
         static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+        {         
             ApplicationConfiguration.Initialize();
             Application.Run(new FormSecureService());
         }
